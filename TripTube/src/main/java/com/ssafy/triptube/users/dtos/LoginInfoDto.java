ce68a9e1aa792e79b7bcfb0d26a54f5f00a5dffe
u__nit <u__nit@naver.com>
2023-05-22 17:34:12 +0900

    refactor: Email info response

--- TripTube/src/main/java/com/ssafy/triptube/users/dtos/LoginInfoDto.java
+++ TripTube/src/main/java/com/ssafy/triptube/users/dtos/LoginInfoDto.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginInfoDto {
 	private String token;
+	private String email;
 	private String name;
 	private String profilePhotoUrl;
 }
