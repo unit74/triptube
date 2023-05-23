8c9fc3cf6fead21650429304d02e619400ad49fb
u__nit <u__nit@naver.com>
2023-05-23 15:57:27 +0900

    refactor: Move URL mapping

--- TripTube/src/main/java/com/ssafy/triptube/trips/attractions/controllers/AttractionPrivateController.java
+++ TripTube/src/main/java/com/ssafy/triptube/trips/attractions/controllers/AttractionPrivateController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.triptube.trips.attractions.services.AttractionInfoService;
+import com.ssafy.triptube.trips.histories.models.HistoryEntity;
+import com.ssafy.triptube.trips.histories.services.HistoryService;
+import com.ssafy.triptube.trips.libraries.services.LibraryService;
+import com.ssafy.triptube.trips.reactions.services.ReactionService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,11 +20,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AttractionPrivateController {
 
-	private final AttractionInfoService attractionInfoService;
+	private final HistoryService historyService;
+
+	private final ReactionService reactionService;
+
+	private final LibraryService libraryService;
+
+	@GetMapping("/histories")
+	public ResponseEntity<?> getHistories(String type, Integer page) {
+		if (!("VISIT".equals(type) || "SEARCH".equals(type))) {
+			return createResponse(false, "검색 기록은 VISIT, SEARCH만 가능");
+		}
+
+		return createResponse(true, type + " 기록 조회",
+				historyService.getHistories(getLoginUserId(), HistoryEntity.Type.valueOf(type), page));
+	}
 
 	@GetMapping("/likes")
 	public ResponseEntity<?> getLikeAttractions(Integer page) {
-		return createResponse(true, "좋아요한 관광지", attractionInfoService.getLikeAttractions(getLoginUserId(), page));
+		return createResponse(true, "좋아요한 관광지", reactionService.getLikeAttractions(getLoginUserId(), page));
+	}
+
+	@GetMapping("/libraries")
+	public ResponseEntity<?> getSaveAttractions(Integer page) {
+		return createResponse(true, "보관한 관광지", libraryService.getSaveLibraries(getLoginUserId(), page));
 	}
 
 }
