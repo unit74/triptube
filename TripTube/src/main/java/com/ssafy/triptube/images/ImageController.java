a6346319761916e15405924ae8c33aca8330af3e
u__nit <u__nit@naver.com>
2023-05-25 15:54:13 +0900

    feat: image

--- TripTube/src/main/java/com/ssafy/triptube/images/ImageController.java
+++ TripTube/src/main/java/com/ssafy/triptube/images/ImageController.java
@@ -13,11 +13,12 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/public/images")
+@RequestMapping("/api/v1/public")
 public class ImageController {
 
 	@Value("${app.fileupload.uploadDir}")
@@ -26,24 +27,38 @@ public class ImageController {
 	@Value("${app.fileupload.uploadPath}")
 	private String uploadPath;
 
-	@GetMapping(value = "/logo", produces = MediaType.IMAGE_PNG_VALUE)
+	@GetMapping(value = "/images/logo", produces = MediaType.IMAGE_PNG_VALUE)
 	public ResponseEntity<?> getLogo() throws IOException {
-		Path file = Paths.get(uploadPath + File.separator + uploadFolder, "logo.png");
-		InputStreamResource resource = new InputStreamResource(Files.newInputStream(file));
-
-		InputStream image = resource.getInputStream();
+		InputStream image = getImage("logo.png");
 
 		return ResponseEntity.ok().body(IOUtils.toByteArray(image));
 	}
 
-	@GetMapping(value = "/no-images", produces = MediaType.IMAGE_PNG_VALUE)
+	@GetMapping(value = "/images/no-images", produces = MediaType.IMAGE_PNG_VALUE)
 	public ResponseEntity<?> getNoImage() throws IOException {
-		Path file = Paths.get(uploadPath + File.separator + uploadFolder, "no-images.png");
-		InputStreamResource resource = new InputStreamResource(Files.newInputStream(file));
-
-		InputStream image = resource.getInputStream();
+		InputStream image = getImage("no-images.png");
 
 		return ResponseEntity.ok().body(IOUtils.toByteArray(image));
 	}
 
+	@GetMapping(value = "/users/profiles/{filename:.+}", produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<?> getProfileImage(@PathVariable String filename) throws IOException {
+		InputStream image = getImage(filename);
+
+		return ResponseEntity.ok().body(IOUtils.toByteArray(image));
+	}
+
+	@GetMapping(value = "/images/contents/{filename:.+}", produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<?> getContentsImage(@PathVariable String filename) throws IOException {
+		InputStream image = getImage(filename);
+
+		return ResponseEntity.ok().body(IOUtils.toByteArray(image));
+	}
+
+	private InputStream getImage(String filename) throws IOException {
+		Path file = Paths.get(uploadPath + File.separator + uploadFolder, filename);
+		InputStreamResource resource = new InputStreamResource(Files.newInputStream(file));
+
+		return resource.getInputStream();
+	}
 }
