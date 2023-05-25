020af1e1bf9540701ad0ad49782743252cf1ba21
rmsah5368 <rmsah5368@naver.com>
2023-05-25 17:59:14 +0900

    집가자

--- enjoytrip/src/services/Api.js
+++ enjoytrip/src/services/Api.js
@@ -17,9 +17,19 @@ export default () => {
 
   axiosInstance.interceptors.response.use(
     (response) => response,
-    (error) => {
-      if (error.response.status === 401 || error.response.status === 403) {
-        // if (error.response.status === 401) {
+    async (error) => {
+      if (error.response.status === 401) {
+        try {
+          const response = await axios.post("/api/v1/public/users/reissue");
+          localStorage.setItem("token", response.data.data);
+          axiosInstance.defaults.headers.common.Authorization = `${response.data.data}`;
+          return axios(error.config);
+        } catch (error) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          location.reload();
+        }
+      } else if (error.response.status === 403) {
         localStorage.removeItem("token");
         localStorage.removeItem("user");
         location.reload();
@@ -30,6 +40,38 @@ export default () => {
 
   return axiosInstance;
 };
+// import axios from "axios";
+
+// export default () => {
+//   const axiosInstance = axios.create({
+//     baseURL: "http://192.168.203.120:8080",
+//     // baseURL: "http://192.168.203.119:8080",
+
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     axiosInstance.defaults.headers.common.Authorization = `${token}`;
+//   }
+
+//   axiosInstance.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//       if (error.response.status === 401 || error.response.status === 403) {
+//         // if (error.response.status === 401) {
+//         localStorage.removeItem("token");
+//         localStorage.removeItem("user");
+//         location.reload();
+//       }
+//       return Promise.reject(error);
+//     }
+//   );
+
+//   return axiosInstance;
+// };
 // export default axios.create({
 //   baseURL: "http://localhost:8080",
 //   headers: {
